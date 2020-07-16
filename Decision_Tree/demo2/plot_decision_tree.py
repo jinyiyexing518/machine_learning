eef5077fb26f95552ae0f2bb879aefc1944a2e0e
jinyiyexing518 <178970674@qq.com>
2020-07-16 12:46:14 +0800

    Random_Forest first submit

--- Decision_Tree/demo2/plot_decision_tree.py
+++ Decision_Tree/demo2/plot_decision_tree.py
@@ -19,18 +19,18 @@ Returns:
 """
 # 函数说明:计算给定数据集的经验熵(香农熵)
 def calcShannonEnt(dataSet):
-    numEntires = len(dataSet)                       #返回数据集的行数
-    labelCounts = {}                               #保存每个标签(Label)出现次数的字典
-    for featVec in dataSet:                        #对每组特征向量进行统计
-        currentLabel = featVec[-1]                 #提取标签(Label)信息
-        if currentLabel not in labelCounts.keys(): #如果标签(Label)没有放入统计次数的字典,添加进去
+    numEntires = len(dataSet)                      # 返回数据集的行数
+    labelCounts = {}                               # 保存每个标签(Label)出现次数的字典
+    for featVec in dataSet:                        # 对每组特征向量进行统计
+        currentLabel = featVec[-1]                 # 提取标签(Label)信息
+        if currentLabel not in labelCounts.keys(): # 如果标签(Label)没有放入统计次数的字典,添加进去
             labelCounts[currentLabel] = 0
-        labelCounts[currentLabel] += 1             #Label计数
-    shannonEnt = 0.0                               #经验熵(香农熵)
-    for key in labelCounts:                        #计算香农熵
-        prob = float(labelCounts[key]) / numEntires#选择该标签(Label)的概率
-        shannonEnt -= prob * log(prob, 2)          #利用公式计算
-    return shannonEnt                              #返回经验熵(香农熵)
+        labelCounts[currentLabel] += 1             # Label计数
+    shannonEnt = 0.0                               # 经验熵(香农熵)
+    for key in labelCounts:                        # 计算香农熵
+        prob = float(labelCounts[key]) / numEntires# 选择该标签(Label)的概率
+        shannonEnt -= prob * log(prob, 2)          # 利用公式计算
+    return shannonEnt                              # 返回经验熵(香农熵)
 
 """
 Parameters:
@@ -41,7 +41,7 @@ Returns:
 """
 # 函数说明:创建测试数据集
 def createDataSet():
-    dataSet = [[0, 0, 0, 0, 'no'],#数据集
+    dataSet = [[0, 0, 0, 0, 'no'], # 数据集
             [0, 0, 0, 1, 'no'],
             [0, 1, 0, 1, 'yes'],
             [0, 1, 1, 0, 'yes'],
@@ -56,8 +56,8 @@ def createDataSet():
             [2, 1, 0, 1, 'yes'],
             [2, 1, 0, 2, 'yes'],
             [2, 0, 0, 0, 'no']]
-    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']#特征标签
-    return dataSet, labels#返回数据集和分类属性
+    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']# 特征标签
+    return dataSet, labels# 返回数据集和分类属性
 
 """
 Parameters:
@@ -69,13 +69,13 @@ Returns:
 """
 # 函数说明:按照给定特征划分数据集
 def splitDataSet(dataSet, axis, value):
-    retDataSet = []                                #创建返回的数据集列表
-    for featVec in dataSet:                        #遍历数据集
+    retDataSet = []                                # 创建返回的数据集列表
+    for featVec in dataSet:                        # 遍历数据集
         if featVec[axis] == value:
-            reducedFeatVec = featVec[:axis]        #去掉axis特征
-            reducedFeatVec.extend(featVec[axis+1:])#将符合条件的添加到返回的数据集
+            reducedFeatVec = featVec[:axis]        # 去掉axis特征
+            reducedFeatVec.extend(featVec[axis+1:])# 将符合条件的添加到返回的数据集
             retDataSet.append(reducedFeatVec)
-    return retDataSet                              #返回划分后的数据集
+    return retDataSet                              # 返回划分后的数据集
 
 """
 Parameters:
@@ -85,25 +85,25 @@ Returns:
 """
 # 函数说明:选择最优特征
 def chooseBestFeatureToSplit(dataSet):
-    numFeatures = len(dataSet[0]) - 1                     #特征数量
-    baseEntropy = calcShannonEnt(dataSet)                 #计算数据集的香农熵
-    bestInfoGain = 0.0                                    #信息增益
-    bestFeature = -1                                      #最优特征的索引值
-    for i in range(numFeatures):                          #遍历所有特征
+    numFeatures = len(dataSet[0]) - 1                     # 特征数量
+    baseEntropy = calcShannonEnt(dataSet)                 # 计算数据集的香农熵
+    bestInfoGain = 0.0                                    # 信息增益
+    bestFeature = -1                                      # 最优特征的索引值
+    for i in range(numFeatures):                          # 遍历所有特征
         #获取dataSet的第i个所有特征
         featList = [example[i] for example in dataSet]
-        uniqueVals = set(featList)                         #创建set集合{},元素不可重复
-        newEntropy = 0.0                                   #经验条件熵
-        for value in uniqueVals:                           #计算信息增益
-            subDataSet = splitDataSet(dataSet, i, value)   #subDataSet划分后的子集
-            prob = len(subDataSet) / float(len(dataSet))   #计算子集的概率
-            newEntropy += prob * calcShannonEnt(subDataSet)#根据公式计算经验条件熵
-        infoGain = baseEntropy - newEntropy                #信息增益
-        # print("第%d个特征的增益为%.3f" % (i, infoGain))   #打印每个特征的信息增益
-        if (infoGain > bestInfoGain):                      #计算信息增益
-            bestInfoGain = infoGain                        #更新信息增益，找到最大的信息增益
-            bestFeature = i                                #记录信息增益最大的特征的索引值
-    return bestFeature                                     #返回信息增益最大的特征的索引值
+        uniqueVals = set(featList)                         # 创建set集合{},元素不可重复
+        newEntropy = 0.0                                   # 经验条件熵
+        for value in uniqueVals:                           # 计算信息增益
+            subDataSet = splitDataSet(dataSet, i, value)   # subDataSet划分后的子集
+            prob = len(subDataSet) / float(len(dataSet))   # 计算子集的概率
+            newEntropy += prob * calcShannonEnt(subDataSet)# 根据公式计算经验条件熵
+        infoGain = baseEntropy - newEntropy                # 信息增益
+        # print("第%d个特征的增益为%.3f" % (i, infoGain))   # 打印每个特征的信息增益
+        if (infoGain > bestInfoGain):                      # 计算信息增益
+            bestInfoGain = infoGain                        # 更新信息增益，找到最大的信息增益
+            bestFeature = i                                # 记录信息增益最大的特征的索引值
+    return bestFeature                                     # 返回信息增益最大的特征的索引值
 
 """
 Parameters:
@@ -114,11 +114,11 @@ Returns:
 # 函数说明:统计classList中出现此处最多的元素(类标签)
 def majorityCnt(classList):
     classCount = {}
-    for vote in classList:#统计classList中每个元素出现的次数
+    for vote in classList:# 统计classList中每个元素出现的次数
         if vote not in classCount.keys():classCount[vote] = 0
         classCount[vote] += 1
-    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)#根据字典的值降序排序
-    return sortedClassCount[0][0]#返回classList中出现次数最多的元素
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)# 根据字典的值降序排序
+    return sortedClassCount[0][0]# 返回classList中出现次数最多的元素
 
 """
 Parameters:
